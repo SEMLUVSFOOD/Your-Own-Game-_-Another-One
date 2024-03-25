36bf824390f74e58c57f2e5612e09a0026ebc906
Sem <44582696+SEMLUVSFOOD@users.noreply.github.com>
2024-03-25 16:16:31 +0100

    Remade the level tester with one way collider and fixed jumping not working

--- Assets/Scripts/PlayerMovement/MoveWASD.cs
+++ Assets/Scripts/PlayerMovement/MoveWASD.cs
@@ -42,18 +42,15 @@ public class MoveWASD : MonoBehaviour
 
         if (Input.GetKeyUp(KeyCode.W) && isJumping == false) {
             rb.AddForce(new Vector2(rb.velocity.x, jump));
+            isJumping = true;
         }
+        
     }
 
     private void OnCollisionEnter2D(Collision2D other) {
         if (other.gameObject.CompareTag("Floor")) {
             isJumping = false;
-        }
-    }
-
-     private void OnCollisionExit2D(Collision2D other) {
-        if (other.gameObject.CompareTag("Floor")) {
-            isJumping = true;
+            Debug.Log("on floor");
         }
     }
 }
