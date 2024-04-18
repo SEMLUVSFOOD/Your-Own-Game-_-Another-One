11388fb1a013ae91d53b97244daea39844377134
Sem <44582696+SEMLUVSFOOD@users.noreply.github.com>
2024-04-18 14:46:29 +0200

    Player 2 animation added

--- Assets/Scripts/PlayerMovement/MoveArrows.cs
+++ Assets/Scripts/PlayerMovement/MoveArrows.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class MoveArrows : MonoBehaviour
 {
@@ -12,14 +13,21 @@ public class MoveArrows : MonoBehaviour
     private float MovementX;
     public Rigidbody2D rb;
 
-    public bool isJumping;
+    public bool isGrounded;
     private bool isLadder;
+
+    Animator animator;
+    SpriteRenderer spriteRenderer; // Add this line
 
     // Start is called before the first frame update
     void Start()
     {
+        animator = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
         MovementX = 0;
+
+        // Get the SpriteRenderer component
+        spriteRenderer = GetComponent<SpriteRenderer>(); // Add this line
     }
 
     // Update is called once per frame
@@ -27,6 +35,16 @@ public class MoveArrows : MonoBehaviour
     {
         // Set velocity based on input and speed
         rb.velocity = new Vector2(MovementX * speed * Time.deltaTime, rb.velocity.y);
+
+        // Flip the character's sprite based on movement direction
+        if (MovementX < 0) // Moving left
+        {
+            spriteRenderer.flipX = true;
+        }
+        else if (MovementX > 0) // Moving right
+        {
+            spriteRenderer.flipX = false;
+        }
 
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
@@ -43,18 +61,23 @@ public class MoveArrows : MonoBehaviour
             MovementX = 0; // Stop movement when arrow keys are released
         }
 
-        if (Input.GetKeyDown(KeyCode.UpArrow) && !isJumping && !isLadder) 
+        if (Input.GetKeyDown(KeyCode.UpArrow) && isGrounded && !isLadder) 
         {
-            rb.AddForce(new Vector2(rb.velocity.x, jump));
-            isJumping = true;
+            rb.AddForce(new Vector2(0f, jump));
+            isGrounded = false;
+            animator.SetBool("isJumping", !isGrounded);
         }
         else if(Input.GetKey(KeyCode.UpArrow) && isLadder)
         {
             rb.velocity = new Vector2(rb.velocity.x, climbingSpeed * Time.deltaTime);
+            animator.SetBool("isLadderAnimation", true);
+
         }
         else if(Input.GetKey(KeyCode.DownArrow) && isLadder)
         {
             rb.velocity = new Vector2(rb.velocity.x, climbingSpeed * Time.deltaTime * -1f);
+            animator.SetBool("isLadderAnimation", true);
+            
         }
         else if(isLadder)
         {
@@ -64,9 +87,16 @@ public class MoveArrows : MonoBehaviour
         
     }
 
+    private void FixedUpdate () {
+        animator.SetFloat("xVelocity", Math.Abs(rb.velocity.x));
+        animator.SetFloat("yVelocity", rb.velocity.y);
+
+    }
+
     private void OnCollisionEnter2D(Collision2D other) {
         if (other.gameObject.CompareTag("Floor")) {
-            isJumping = false;
+            isGrounded = true;
+            animator.SetBool("isJumping", !isGrounded);
         }
     }
 
@@ -76,6 +106,8 @@ public class MoveArrows : MonoBehaviour
             isLadder = true;
             Debug.Log("in ladder");
         }
+        
+
     }
 
     private void OnTriggerExit2D(Collider2D other) {
@@ -83,6 +115,8 @@ public class MoveArrows : MonoBehaviour
             rb.gravityScale = 5f;
             isLadder = false;
             Debug.Log("off ladder");
+            animator.SetBool("isLadderAnimation", false);
+
         }
     }
 }
